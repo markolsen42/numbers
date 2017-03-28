58297df29e7ab2d9b65c5b4ef35cb6aa0b60815c
DATACOM-NZ\markol <markol@datacom.co.nz>
2017-03-29 10:22:46 +1300

    add some simple number theory

--- numbers/src/app/divisible/divisible.component.ts
+++ numbers/src/app/divisible/divisible.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DivisibleComponent implements OnInit {
 
+public numerator:number = 2;
+  public denominator:number = 1;
+  public isDivisible:boolean = true;
+  public factors:number[] = [];
+  public isPrime: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
+    this.doCalculations()
   }
+
+  doCalculations(){
+    this.calculateDivisible();
+    this.calculateFactors();
+    this.calculateIsPrime()
+  }
+
+  calculateDivisible(){
+    this.isDivisible = (this.numerator%this.denominator==0)
+  }
+
+  calculateFactors(){
+    this.factors = [1];
+    for (let i=2;i<=this.numerator/2;i++){
+      if (this.numerator%i==0){
+        this.factors.push(i)
+      }
+    }
+    this.factors.push(this.numerator);
+  }
+
+  calculateIsPrime(){
+    this.isPrime = this.factors.length==2;
+  }
+
 
 }
